24bbbf2053cf24e67152c3aa285d2a9d27d0ab53
Pratiks-LAB <pratikvish1998@gmail.com>
2026-08-20 15:21:21 +0530

    Update checkoutApp.groovy

--- vars/checkoutApp.groovy
+++ vars/checkoutApp.groovy
@@ -1,9 +1,1 @@
-def call(){
-  stage('Checkout') {
-    steps {
-      git branch: 'main',
-        credentialsId: '1f418500-b4c7-43ae-83ba-2e48f4342da3',
-        url: 'https://github.com/Pratiks-LAB/sample-webapp.git'
-    }
-  }
-}
+
